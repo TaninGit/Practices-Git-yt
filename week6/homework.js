365102bd857efa930fa2fe5138fc798893515eef
TaninGit <chieangtanin@gmail.com>
2024-09-21 23:55:27 +0700

    add practices

---
+++ week6/homework.js
@@ -0,0 +1,14 @@
+// Practices 
+function extractAndRename(obj) {
+    return {fullname: obj['name'], yearOld: obj['age']}
+}
+console.log(extractAndRename({name:'John', age: 25, city:"new york"}))
+console.log(extractAndRename({name:'John', age: 30}))
+
+// Practices
+function mergeAndDestructure(obj1,obj2){
+    const result = {...obj1,...obj2}
+    return {fullname: result['name'], yearOld: result['age']}
+}
+console.log(mergeAndDestructure({name:'John', age: 25}, {city:"new york"}))
+console.log(mergeAndDestructure({name:'John'}, { age: 30,city:"USA"}))
